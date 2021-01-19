cbfa27faff7bd35af184d9eabceb595571fd6b54
Gianni Trevisiol <gtrevi05@hotmail.com.com>
2021-01-19 19:31:23 +0100

    Removed text from heap_tracker_lib.h (repeats what already in the readme.md)

--- HeapTracker/heap_tracker_lib.h
+++ HeapTracker/heap_tracker_lib.h
@@ -4,30 +4,6 @@
 * Licensed under the MIT License.
 */
 
-/*
-* 
-* Purpose of this library *
-* This library allows to override the native C malloc() and free(), in order to allow implementing custom heap tracking
-* through a global 'heap_allocated' variable, which can be used to prevent OOM conditions.
-*
-* Context *
-* Under the default memory management strategy, the Linux's kernel follows an "optimistic memory allocation strategy",
-* that is, the kernel will not "actually" allocate the requested memory until you actually try to use it. 
-* Under this strategy, mallocs will *always* succeed and return a non-NULL pointer and as per Linux's overcommitment policy,
-* under extremely low memory conditions the kernel will start firing off the "OOM Killer" routine.
-* Processes are given "OOM scores" and the process with the highest score will get killed.
-* Therefore, by design in optimistic strategy, an App will crash with "Child terminated with signal = 0x9 (SIGKILL)" 
-* when the heap could not "effectively" allocate the requested memory, upon its first access.
-*  
-* Usage *
-* 1) Compile the library by adding the following two options to target_link_libraries() in CMakeLists.txt: -Wl,--wrap malloc -Wl,--wrap,free
-*    i.e.: target_link_libraries(${PROJECT_NAME} applibs pthread gcc_s c -Wl,--wrap=malloc -Wl,--wrap=free)
-* 2) Determine, by trial & error, what is the maximum heap usage for your App, and set the 'heap_limit' constant accordingly.
-* 3) Use malloc() as usual in your App, BUT use the _free() and _realloc() helpers, in order to keep track of the available heap.
-*    If the native free() and realloc() functions will be used in the App, then the internal heap counter will become invalid, and the
-*    'heap_allocated' counter will become unreliable
-*/
-
 #include <stdio.h>
 
 //////////////////////////////////////////////////////////////////////////////////
