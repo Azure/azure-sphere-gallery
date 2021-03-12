3a4977d08078b189c12c7dbc19135710b0ea5c3f
Gianni Trevisiol <gtrevi@users.noreply.github.com>
2021-03-12 17:37:40 +0100

    Added OSNetworkRequirementChecker-PC tool.
    Initial commit.

---
+++ OSNetworkRequirementChecker-PC/README.md
@@ -0,0 +1,139 @@
+# OSNetworkRequirementChecker-PC
+
+This PC command line utility enables you to test some aspects of the [Azure Sphere OS Networking Requirements](https://docs.microsoft.com/azure-sphere/network/ports-protocols-domains) using a PC attached to the network that you wish to attach an Azure Sphere device to. This makes it easier to determine if a given network environment does not meet Azure Sphere OS's networking requirements, which can cause issues such as lack of time synchronization, inability to receive OS or Application updates, and lack of short-lived device certificate which means that an application's Azure IoT connections may not work properly. 
+
+This application tests the following:
+
+1. For the standard NTP endpoints used by the OS, it verifies if a correct time-sync can be retrieved from them, using the same port numbers as the Azure Sphere OS uses.
+
+2. For the list of other endpoints that the OS communicates with, it verifies that the hostnames can be resolved via DNS to IP addresses, and verifies that a TCP connection can be established to those endpoints using the correct port number.
+
+During execution, the utility displays whether each check fails or succeeds.
+
+## Contents
+| File/folder | Description |
+|-------------|-------------|
+| `main.cpp` | The console application source code. |
+| `README.md` | This README file. |
+| `LICENSE.txt` | The license for the project. |
+
+## Prerequisites
+
+1. Internet connection through Wi-Fi or Ethernet.
+2. A PC connected to the same network, to run the utility on.
+
+## Setup
+
+Copy the all the project files in a working directory of your choice, from now on referred as `<WRKDIR>`.
+
+## Running the code
+
+### Build and run the app
+The command line utility has a single source file which supports compiling on Windows (i.e. Visual Studio or Visual Studio Code) and Linux (i.e. Visual Studio Code or g++):
+
+#### On Windows (Visual Studio)
+Open the `OSNetworkRequirementChecker-PC.sln` and build it. The executable should be generated in the following directory:
+```
+<WRKDIR>\x64\Debug\OSNetworkRequirementChecker-PC.exe
+```
+
+#### On Linux (g++)
+Open a terminal window and navigate to your `<WRKDIR>`, and run the following command
+
+```
+~/<WRKDIR> $ g++ -o OSNetworkRequirementChecker-PC main.cpp
+```
+Once compiled, the only required executable `OSNetworkRequirementChecker-PC` will be located in the same directory where the command was launched.
+
+### Testing the app
+To run the application, you just need the single executable file produced from the previous steps:
+#### On Windows
+```
+.\<WRKDIR>\x64\Debug>OSNetworkRequirementChecker-PC.exe
+```
+#### On Linux
+```
+sudo ./OSNetworkRequirementChecker-PC
+```
+When you run the application, it'll display the checks being executed and their related results as they are performed.
+A successful output will look the following (errors will be displayed inline in case they occur):
+```     
+        Azure Sphere network-checkup utility.
+
+        Querying required NTP servers...
+        - time from time.windows.com<51.105.208.173> --> Fri Mar 12 12:17:35 2021
+        - time from time.sphere.azure.net<51.105.208.173> --> Fri Mar 12 12:17:36 2021
+        - time from prod.time.sphere.azure.net<51.105.208.173> --> Fri Mar 12 12:17:36 2021
+        - time from 13.86.101.172<13.86.101.172> --> Fri Mar 12 12:17:36 2021
+        - time from 20.43.94.199<20.43.94.199> --> Fri Mar 12 12:17:36 2021
+        - time from 20.189.79.72<20.189.79.72> --> Fri Mar 12 12:17:36 2021
+        - time from 40.81.94.65<40.81.94.65> --> Fri Mar 12 12:17:36 2021
+        - time from 40.81.188.85<40.81.188.85> --> Fri Mar 12 12:17:37 2021
+        - time from 40.119.6.228<40.119.6.228> --> Fri Mar 12 12:17:37 2021
+        - time from 51.105.208.173<51.105.208.173> --> Fri Mar 12 12:17:37 2021
+        - time from 51.137.137.111<51.137.137.111> --> Fri Mar 12 12:17:37 2021
+        - time from 51.145.123.29<51.145.123.29> --> Fri Mar 12 12:17:37 2021
+        - time from 52.148.114.188<52.148.114.188> --> Fri Mar 12 12:17:37 2021
+        - time from 52.231.114.183<52.231.114.183> --> Fri Mar 12 12:17:37 2021
+
+        Querying required hostnames...
+
+        Device provisioning and communication with IoT Hub:
+        Resolving global.azure-devices-provisioning.net... success --> connecting to 40.79.180.98:8883...  success!
+        Resolving global.azure-devices-provisioning.net... success --> connecting to 40.79.180.98:443...  success!
+
+        Internet connection checks, certificate file downloads, and similar tasks:
+        Resolving www.msftconnecttest.com... success --> connecting to 13.107.4.52:80...  success!
+        Resolving prod.update.sphere.azure.net... success --> connecting to 152.199.19.161:80...  success!
+
+        Communication with web services and Azure Sphere Security service:
+        Resolving anse.azurewatson.microsoft.com... success --> connecting to 52.165.226.185:443...  success!
+        Resolving eastus-prod-azuresphere.azure-devices.net... success --> connecting to 137.117.83.38:443...  success!
+        Resolving prod.core.sphere.azure.net... success --> connecting to 13.83.64.76:443...  success!
+        Resolving prod.device.core.sphere.azure.net... success --> connecting to 13.87.216.151:443...  success!
+        Resolving prod.deviceauth.sphere.azure.net... success --> connecting to 52.146.68.47:443...  success!
+        Resolving prod.dinsights.core.sphere.azure.net... success --> connecting to 52.188.78.215:443...  success!
+        Resolving prod.releases.sphere.azure.net... success --> connecting to 13.107.246.19:443...  success!
+        Resolving prodmsimg.blob.core.windows.net... success --> connecting to 52.239.154.132:443...  success!
+        Resolving prodmsimg-secondary.blob.core.windows.net... success --> connecting to 52.239.162.36:443...  success!
+        Resolving prodptimg.blob.core.windows.net... success --> connecting to 52.240.48.36:443...  success!
+        Resolving prodptimg-secondary.blob.core.windows.net... success --> connecting to 52.239.104.36:443...  success!
+        Resolving sphereblobeus.azurewatson.microsoft.com... success --> connecting to 13.107.246.19:443...  success!
+        Resolving sphereblobweus.azurewatson.microsoft.com... success --> connecting to 13.107.246.19:443...  success!
+        Resolving sphere.sb.dl.delivery.mp.microsoft.com... success --> connecting to 152.199.21.175:443...  success!
+
+        Please see https://docs.microsoft.com/en-us/azure-sphere/network/ports-protocols-domains for further details.
+```
+
+## Next steps
+
+### Project expectations
+
+For the latest information regarding the network connectivity requirements for Azure Sphere-based devices, please refer to the official [Azure Sphere OS networking requirements](https://docs.microsoft.com/en-us/azure-sphere/network/ports-protocols-domains) documentation.
+
+### Expected support for the code
+
+There is no official support guarantee for this code, but we will make a best effort to respond to/address any issues you encounter.
+
+### How to report an issue
+
+If you run into an issue with this script, please open a GitHub issue against this repo.
+
+## Contributing
+
+This project welcomes contributions and suggestions. Most contributions require you to
+agree to a Contributor License Agreement (CLA) declaring that you have the right to,
+and actually do, grant us the rights to use your contribution. For details, visit
+https://cla.microsoft.com.
+
+When you submit a pull request, a CLA-bot will automatically determine whether you need
+to provide a CLA and decorate the PR appropriately (e.g., label, comment). Simply follow the
+instructions provided by the bot. You will only need to do this once across all repositories using our CLA.
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
+For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/)
+or contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+
+## License
+
+For information about the licenses that apply to this script, see [LICENSE.txt](./LICENCE.txt)
---
+++ OSNetworkRequirementChecker-PC/main.cpp
@@ -0,0 +1,346 @@
+#if defined(_WIN32) || defined(__WIN32__) || defined(_MSC_VER)
+#	define _WINSOCK_DEPRECATED_NO_WARNINGS
+#	ifndef _CRT_SECURE_NO_WARNINGS
+#	define _CRT_SECURE_NO_WARNINGS
+#	endif /* _CRT_SECURE_NO_WARNINGS */
+#	pragma comment(lib, "Ws2_32.lib")
+#	include <WS2tcpip.h>
+#	include <winsock2.h>
+#else
+#	include <sys/types.h>
+#	include <sys/socket.h>
+#   include <unistd.h>
+#	include <netinet/in.h>
+#	include <arpa/inet.h>
+#	include <netdb.h>
+#endif
+
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <iostream>
+
+const char* const ntpServers[] =
+{
+	"time.windows.com",
+	"time.sphere.azure.net",
+	"prod.time.sphere.azure.net",
+	"13.86.101.172",  "20.43.94.199",   "20.189.79.72",
+	"40.81.94.65",    "40.81.188.85",   "40.119.6.228",
+	"51.105.208.173", "51.137.137.111", "51.145.123.29",
+	"52.148.114.188", "52.231.114.183",
+	""
+};
+
+typedef struct 
+{
+	const char *hostname;
+	int port;
+} t_endpoints;
+const t_endpoints endpoints[] =
+{
+	// Device provisioning and communication with IoT Hub
+	{ "Device provisioning and communication with IoT Hub:", -1},
+	{ "global.azure-devices-provisioning.net", 8883},
+	{ "global.azure-devices-provisioning.net", 443}, // websocket
+
+	// Internet connection checks, certificate file downloads, and similar tasks
+	{ "Internet connection checks, certificate file downloads, and similar tasks:", -1},
+	{ "www.msftconnecttest.com", 80},
+	{ "prod.update.sphere.azure.net", 80},
+
+	// Communication with web services and Azure Sphere Security service
+	{ "Communication with web services and Azure Sphere Security service:", -1},
+	{ "anse.azurewatson.microsoft.com", 443},
+	{ "eastus-prod-azuresphere.azure-devices.net", 443},
+	{ "prod.core.sphere.azure.net", 443},
+	{ "prod.device.core.sphere.azure.net", 443},
+	{ "prod.deviceauth.sphere.azure.net", 443},
+	{ "prod.dinsights.core.sphere.azure.net", 443},
+	{ "prod.releases.sphere.azure.net", 443},
+	{ "prodmsimg.blob.core.windows.net", 443},
+	{ "prodmsimg-secondary.blob.core.windows.net", 443},
+	{ "prodptimg.blob.core.windows.net", 443},
+	{ "prodptimg-secondary.blob.core.windows.net", 443},
+	{ "sphereblobeus.azurewatson.microsoft.com", 443},
+	{ "sphereblobweus.azurewatson.microsoft.com", 443},
+	{ "sphere.sb.dl.delivery.mp.microsoft.com", 443},
+	{ "", 0}
+};
+
+
+
+#define NTP_PORT			123				// NTP standard listening port
+#define NTP_PORT_OUT		124				// NTP requests from Azure Sphere are sourced through local port 124
+#define NTP_TIMESTAMP_DELTA 2208988800ull	// 70 years in seconds
+
+typedef struct
+{
+	union
+	{
+		struct
+		{
+			uint8_t mode : 3;	// mode - i.e. client will pick mode 3 for client
+			uint8_t vn : 3;		// vn	- Version number of the protocol.
+			uint8_t li : 2;		// li	- Leap indicator
+		};	
+		uint8_t li_vn_mode;		// 8 bits. li, vn, and mode.							 
+	};
+
+	uint8_t stratum;         // 8 bits. Stratum level of the local clock.
+	uint8_t poll;            // 8 bits. Maximum interval between successive messages.
+	uint8_t precision;       // 8 bits. Precision of the local clock.
+
+	uint32_t rootDelay;      // 32 bits. Total round trip delay time.
+	uint32_t rootDispersion; // 32 bits. Max error aloud from primary clock source.
+	uint32_t refId;          // 32 bits. Reference clock identifier.
+
+	uint32_t refTm_s;        // 32 bits. Reference time-stamp seconds.
+	uint32_t refTm_f;        // 32 bits. Reference time-stamp fraction of a second.
+
+	uint32_t origTm_s;       // 32 bits. Originate time-stamp seconds.
+	uint32_t origTm_f;       // 32 bits. Originate time-stamp fraction of a second.
+
+	uint32_t rxTm_s;         // 32 bits. Received time-stamp seconds.
+	uint32_t rxTm_f;         // 32 bits. Received time-stamp fraction of a second.
+
+	uint32_t txTm_s;         // 32 bits. Transmit time-stamp seconds (the essential field that the client wll use).
+	uint32_t txTm_f;         // 32 bits. Transmit time-stamp fraction of a second.
+
+} ntp_packet;				 // Total: 384 bits or 48 bytes.
+
+int query_ntp_server(const char *hostname, int ntp_port, int src_port);
+int resolve_hostname(const char *hostname, int port);
+
+
+int getSocketErrorCode(void)
+{
+#if defined(_WIN32) || defined(__WIN32__) || defined(_MSC_VER)
+	// https://docs.microsoft.com/en-us/windows/win32/winsock/windows-sockets-error-codes-2
+	return WSAGetLastError();
+#else
+	return errno;
+#endif
+}
+
+int query_ntp_server(const char *hostname, int ntp_port, int src_port)
+{
+	int iRes = 0;
+	
+	// Build the NTP packet
+	ntp_packet packet = { 0 };
+	packet.li = 0;		// No warning
+	packet.vn = 4;		// Version 4 (aka IPv4-only)
+	packet.mode = 3;	// Client Mode
+
+
+#if defined(_WIN32) || defined(__WIN32__) || defined(_MSC_VER)
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{		
+		iRes = getSocketErrorCode();
+		std::cerr << "WSAStartup() failed with error code " << iRes << std::endl;
+		
+		WSACleanup();
+		return iRes;
+	}
+#endif
+
+	// Does the host exist?
+	struct hostent *server = gethostbyname(hostname);
+	if (server == NULL)
+	{
+		iRes = getSocketErrorCode();
+		std::cerr << "gethostbyname() error " << iRes << std::endl;
+	}
+	else
+	{
+		// Setup the server address
+		struct sockaddr_in server_addr;
+		memset(&server_addr, 0, sizeof(server_addr));
+		server_addr.sin_family = AF_INET;
+		server_addr.sin_addr.s_addr = *(long *)(server->h_addr_list[0]);
+		server_addr.sin_port = htons(ntp_port);
+		char server_ip_addr[INET_ADDRSTRLEN];
+		sprintf(server_ip_addr, "%s", inet_ntoa(*((struct in_addr *)server->h_addr)));
+
+		// Setup the source address (for source port customization)
+		struct sockaddr_in client_addr;
+		memset(&client_addr, 0, sizeof(client_addr));
+		client_addr.sin_family = AF_INET;
+		client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+		// Optionally, set the given outbound port
+		if (src_port > 0)
+		{
+			client_addr.sin_port = htons(src_port);
+		}
+
+		// Open an UDP socket
+		int sock_fd = (int)socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+		if (sock_fd < 0)
+		{
+			iRes = getSocketErrorCode();
+			std::cerr << "socket() open error " << iRes << std::endl;
+		}
+		else
+		{
+			// Bind it to the client w/optional desired source port
+			if (bind(sock_fd, (struct sockaddr *)&client_addr, sizeof(client_addr)) < 0)
+			{
+				iRes = getSocketErrorCode();
+				std::cerr << "bind() failed with error " << iRes << std::endl;
+			}
+			else
+			{
+				// Send the NTP packet
+				int sent_bytes = sendto(sock_fd, (const char *)&packet, sizeof(packet), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+				if (sent_bytes < 0)
+				{
+					iRes = getSocketErrorCode();
+					std::cerr << "sendto() failed with error " << iRes << std::endl;
+				}
+				else
+				{
+					// Get the time data back
+					socklen_t client_addr_len = sizeof(client_addr);
+					int recv_bytes = recvfrom(sock_fd, (char *)&packet, sizeof(packet), 0, (struct sockaddr *)&client_addr, &client_addr_len);
+					if (recv_bytes < 0)
+					{
+						iRes = getSocketErrorCode();
+						std::cerr << "recvfrom() failed with error " << iRes << std::endl;
+					}
+					else
+					{
+						// txTm_s + txTm_f contain the time-stamp in seconds passed since Jan 1st 1900, as the packet left the NTP server.
+						// Firstly, they need to be converted from the network's to the local host's "endianness".
+						packet.txTm_s = ntohl(packet.txTm_s);
+						packet.txTm_f = ntohl(packet.txTm_f);
+
+						// Compute the time-stamp in seconds since NTP epoch (catching both txTm_s + txTm_f in 64-bit integer), 
+						// by subtracting 70 years worth of seconds, which results in the seconds since 00:00:00 UTC on 1 January 1970 (UNIX epoch).
+						time_t txTm = (time_t)((uint64_t)packet.txTm_s - NTP_TIMESTAMP_DELTA);
+
+						// Print the time we got from the server, accounting for local timezone and conversion from UTC time.
+						std::cout << "- time from " << hostname << "<" << server_ip_addr << "> --> " << ctime((const time_t *)&txTm);
+					}
+				}
+			}
+#if defined(_WIN32) || defined(__WIN32__) || defined(_MSC_VER)
+			closesocket(sock_fd);
+#else
+			close(sock_fd);
+#endif
+		}
+	}	
+
+#if defined(_WIN32) || defined(__WIN32__) || defined(_MSC_VER)
+	WSACleanup();
+#endif
+
+	return iRes;
+}
+
+int resolve_hostname(const char *hostname, int port)
+{
+	int iRes = 0;
+
+
+#if defined(_WIN32) || defined(__WIN32__) || defined(_MSC_VER)
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		iRes = getSocketErrorCode();
+		std::cerr << "WSAStartup() failed with error code " << iRes << std::endl;
+
+		WSACleanup();
+		return iRes;
+	}
+#endif
+
+	// Does the host exist?
+	std::cout << "Resolving " << hostname << "... ";
+	struct hostent *server = gethostbyname(hostname);
+	if (server == NULL)
+	{
+		iRes = getSocketErrorCode();
+		std::cout << "FAILED (errno=" << iRes << ")" << std::endl;
+	}
+	else
+	{
+		// Retrieve the server address
+		struct sockaddr_in server_addr;
+		memset(&server_addr, 0, sizeof(server_addr));
+		server_addr.sin_family = AF_INET;
+		server_addr.sin_addr.s_addr = *(long *)(server->h_addr_list[0]);
+
+		// Let's attempt a dummy connection
+		std::cout << "success --> connecting to " << inet_ntoa(*((struct in_addr *)server->h_addr)) << ":" << port << "... ";
+		int sock_fd = (int)socket(AF_INET, SOCK_STREAM, 0);
+		if (sock_fd < 0)
+		{
+			iRes = getSocketErrorCode();
+			std::cerr << "socket() open error " << iRes << std::endl;
+		}
+		else
+		{
+			server_addr.sin_port = htons(port);
+			if (connect(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+			{				
+				iRes = getSocketErrorCode();
+				std::cout << "FAILED (errno=" << iRes << ")" << std::endl;
+			}
+			else
+			{
+#if defined(_WIN32) || defined(__WIN32__) || defined(_MSC_VER)
+				closesocket(sock_fd);
+#else
+				close(sock_fd);
+#endif
+				std::cout << " success!" << std::endl;
+			}
+		}
+	}
+
+#if defined(_WIN32) || defined(__WIN32__) || defined(_MSC_VER)
+	WSACleanup();
+#endif
+
+	return iRes;
+}
+
+int main(int argc, char **argv)
+{
+	std::cout << "Azure Sphere network-checkup utility." << std::endl << std::endl;	
+	try
+	{
+		std::cout << "Querying required NTP servers..." << std::endl;
+		for (int i = 0; *ntpServers[i]; i++)
+		{			
+			query_ntp_server(ntpServers[i], NTP_PORT, NTP_PORT_OUT);
+		}
+
+		std::cout << std::endl << "Querying required hostnames..." << std::endl;
+		for (int i = 0; *endpoints[i].hostname; i++)
+		{
+			if (-1 == endpoints[i].port)
+			{
+				// This entry is just a description
+				std::cout << std::endl << endpoints[i].hostname << std::endl;
+			}
+			else
+			{
+				resolve_hostname(endpoints[i].hostname, endpoints[i].port);
+			}
+		}
+		std::cout << std::endl;
+	}
+	catch (...)
+	{
+		std::cout << std::endl << "Unexpected exception executing checks!" << std::endl;
+	}
+
+	std::cout << "Please see https://docs.microsoft.com/en-us/azure-sphere/network/ports-protocols-domains for further details." << std::endl << std::endl;
+
+	return 0;
+}
