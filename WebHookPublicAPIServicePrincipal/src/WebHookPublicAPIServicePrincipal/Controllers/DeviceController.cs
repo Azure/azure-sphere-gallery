0b16426a007150f786e68526efa9f921d24eda11
Mike Hall <mikehall@microsoft.com>
2021-07-07 13:26:40 +0100

    adding WebHookPublicAPIServicePrincipal project (#45)

---
+++ WebHookPublicAPIServicePrincipal/src/WebHookPublicAPIServicePrincipal/Controllers/DeviceController.cs
@@ -0,0 +1,76 @@
+﻿/* Copyright (c) Microsoft Corporation. All rights reserved.
+   Licensed under the MIT License. */
+
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace WebHookPublicAPIServicePrincipal.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class Root : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return $"API Service is running...";
+        }
+    }
+
+    [Route("[controller]")]
+    [ApiController]
+    public class WebHook : ControllerBase
+    {
+        /// <summary>
+        /// Users Azure Sphere Tenant Id
+        /// </summary>
+        private static string AzureSphereTenantId = "";     // TODO: add your Azure Sphere Tenant Id (azsphere tenant show-selected)
+
+        // GET: used to initialize the Token process
+        [HttpGet]
+        public async Task<string> Get([FromQuery] string DeviceId = "")
+        {
+            if (string.IsNullOrEmpty(DeviceId))
+                return "Bad Request";
+
+            // Get Service Principal Token.
+            string token = await Utils.GetAS3Token();
+
+            // Get some basic information for the device in the Azure Sphere tenant.
+            string deviceData = Utils.GetData($"tenants/{AzureSphereTenantId}/devices/{DeviceId}", token);
+            Debug.WriteLine(deviceData);
+
+            if (string.IsNullOrEmpty(deviceData))
+            {
+                return "Bad Request";
+            }
+
+            DeviceInfo devInfo = JsonConvert.DeserializeObject<DeviceInfo>(deviceData);
+
+            // https://prod.core.sphere.azure.net/v2/tenants/{tenantId}/devicegroups/{deviceGroupId}
+            string deviceGroup = Utils.GetData($"tenants/{AzureSphereTenantId}/devicegroups/{devInfo.DeviceGroupId}", token);
+            Debug.WriteLine(deviceGroup);
+            DeviceGroupInfo dgInfo = JsonConvert.DeserializeObject<DeviceGroupInfo>(deviceGroup);
+
+            // https://prod.core.sphere.azure.net/v2/tenants/{tenantId}/products/{productId}
+            string product = Utils.GetData($"tenants/{AzureSphereTenantId}/products/{devInfo.ProductId}", token);
+            Debug.WriteLine(product);
+            ProductInfo prodInfo = JsonConvert.DeserializeObject<ProductInfo>(product);
+
+            string[] AppUpdatePolicies = new string[] { "Update All", "No 3rd Party App Updates", "No Updates" };
+
+            string ret = $"Device           : {DeviceId}\n" +
+                $"Product          : {prodInfo.Name}\n" +
+                $"Device Group     : {dgInfo.Name}\n" +
+                $"Retail Eval      : {dgInfo.OsFeedType}\n" +
+                $"App Update Policy: {AppUpdatePolicies[dgInfo.UpdatePolicy]}";
+
+            Debug.WriteLine(ret);
+
+            return ret;
+        }
+    }
+}
+
---
+++ WebHookPublicAPIServicePrincipal/src/WebHookPublicAPIServicePrincipal/Utils.cs
@@ -0,0 +1,87 @@
+﻿/* Copyright (c) Microsoft Corporation. All rights reserved.
+   Licensed under the MIT License. */
+
+using Azure.Identity;
+using RestSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace WebHookPublicAPIServicePrincipal
+{
+    public class Utils
+    {
+        /// <summary>
+        /// Azure Sphere Public API URI
+        /// </summary>
+        private const string AzureSphereApiUri = "https://prod.core.sphere.azure.net";
+        public static string GetData(string EndpointUrl, string token)
+        {
+            var request = (RestRequest)null;
+            var client = (RestClient)null;
+
+            // calling an Azure Sphere API
+            if (!string.IsNullOrEmpty(token))
+            {
+                client = new RestClient(AzureSphereApiUri);
+                request = new RestRequest($"/v2/{EndpointUrl}", Method.GET);
+                request.AddParameter("Authorization", string.Format("Bearer " + token), ParameterType.HttpHeader);
+            }
+            else
+            {
+                Uri hostUri = new Uri(EndpointUrl);
+                string hostName = hostUri.Host;
+                string relativePath = hostUri.PathAndQuery;
+                client = new RestClient("https://" + hostName);
+                request = new RestRequest(relativePath, Method.GET);
+            }
+
+            var response = client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                return response.Content;
+            }
+            return string.Empty;
+        }
+
+        public static async Task<string> GetAS3Token()
+        {
+            DefaultAzureCredential credential = new DefaultAzureCredential();
+            var result = await credential.GetTokenAsync(new Azure.Core.TokenRequestContext(
+            new[] { "https://firstparty.sphere.azure.net/api/.default" }));
+
+            return result.Token;
+        }
+    }
+
+    public class DeviceInfo
+    {
+        public string DeviceId { get; set; }
+        public string TenantId { get; set; }
+        public string ProductId { get; set; }
+        public string DeviceGroupId { get; set; }
+        public int ChipSku { get; set; }
+    }
+
+    public class DeviceGroupInfo
+    {
+        public string Id { get; set; }
+        public string TenantId { get; set; }
+        public int OsFeedType { get; set; }
+        public string ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public object CurrentDeployment { get; set; }
+        public int UpdatePolicy { get; set; }
+        public bool AllowCrashDumpsCollection { get; set; }
+    }
+
+    public class ProductInfo
+    {
+        public string Id { get; set; }
+        public string TenantId { get; set; }
+        public string Name { get; set; }
+        public object Description { get; set; }
+    }
+
+}
