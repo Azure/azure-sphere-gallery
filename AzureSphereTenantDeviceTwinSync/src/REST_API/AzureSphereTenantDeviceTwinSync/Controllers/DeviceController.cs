64205a23125f710f84edfc4613e4a66aafd181a6
mikehall <mikehall@microsoft.com>
2021-07-19 13:00:40 +0100

    Updated AzureSphereTenantDeviceTwinSync to add Service Principal support and add AS3 Public API changes

--- AzureSphereTenantDeviceTwinSync/src/REST_API/AzureSphereTenantDeviceTwinSync/Controllers/DeviceController.cs
+++ AzureSphereTenantDeviceTwinSync/src/REST_API/AzureSphereTenantDeviceTwinSync/Controllers/DeviceController.cs
@@ -7,6 +7,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.Devices.Shared;
 
 namespace DevicePropertiesWebHook.Controllers
 {
@@ -14,11 +16,26 @@ namespace DevicePropertiesWebHook.Controllers
     [ApiController]
     public class Root : ControllerBase
     {
+        private IConfiguration _configuration;
+
+        public Root(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet]
         public string Get()
         {
-            string authStatus = ADToken.isAuthComplete() ? "Auth is complete" : "Auth has not completed";
-            return $"API Service is running... {authStatus}";
+            bool useDeviceCodeFlow = _configuration.GetValue<bool>("MySettings:UseDeviceCodeFlow");
+            if (useDeviceCodeFlow)
+            {
+                string authStatus = ADToken.isAuthComplete() ? "Auth is complete" : "Auth has not completed";
+                return $"API Service is running and using Device Code Authentication... {authStatus}";
+            }
+            else
+            {
+                return "API Service is running and using Managed Identity...";
+            }
         }
     }
 
@@ -45,6 +62,12 @@ namespace DevicePropertiesWebHook.Controllers
         /// </summary>
         private static string[] AppUpdatePolicies = new string[] { "Update All", "No 3rd Party App Updates", "No Updates" };
 
+        private IConfiguration _configuration;
+
+        public WebHook(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         // GET: used to initialize the Token process
         [HttpGet]
@@ -55,9 +78,10 @@ namespace DevicePropertiesWebHook.Controllers
 
             Debug.WriteLine($"API Key = {UserAPIKey}");
 
-            APIKey = Utils.GetKeyVaultString("APIKey");
-            IoTHubConnectionString = Utils.GetKeyVaultString("IoTHubConnectionString");
-            AzureSphereTenantId = Utils.GetKeyVaultString("tenantId");
+            if (!GetSecrets())
+            {
+                return "Bad Request";
+            }
 
             if (!APIKey.Equals(UserAPIKey))
             {
@@ -73,23 +97,30 @@ namespace DevicePropertiesWebHook.Controllers
             // also have the user 'Azure Sphere Tenant Id' and IoTHub Connection String
 
             // Initialize IPublicClientApplication
-            ADToken.InitPCA();
-            ManualResetEvent oSignalEvent = new ManualResetEvent(false);
+            bool useDeviceCodeFlow = _configuration.GetValue<bool>("MySettings:UseDeviceCodeFlow");
 
-            ADToken.InitDeviceFlow(oSignalEvent, AzureSphereTenantId);
+            if (useDeviceCodeFlow)
+            {
+                ADToken.InitPCA();
+                ManualResetEvent oSignalEvent = new ManualResetEvent(false);
 
-            oSignalEvent.WaitOne(); //This thread will block here until the reset event is sent.
-            oSignalEvent.Reset();
+                ADToken.InitDeviceFlow(oSignalEvent, AzureSphereTenantId);
 
-            // GetDeviceFlowInstructions might be null if the flow wasn't completed.
-            string instructions = ADToken.GetDeviceFlowInstructions();
+                oSignalEvent.WaitOne(); //This thread will block here until the reset event is sent.
+                oSignalEvent.Reset();
 
-            // Call ADToken function that wraps the token code in a thread
-            // wait on ManualResetEvent to get user string.
+                // GetDeviceFlowInstructions might be null if the flow wasn't completed.
+                string instructions = ADToken.GetDeviceFlowInstructions();
 
-            // normal operation will just call the code to get the token (without the thread)
+                // Call ADToken function that wraps the token code in a thread
+                // wait on ManualResetEvent to get user string.
 
-            return instructions;
+                // normal operation will just call the code to get the token (without the thread)
+
+                return instructions;
+            }
+
+            return "API Service is using Managed Identity";
         }
 
         // POST: the webhook endpoint called from EventGrid
@@ -116,6 +147,10 @@ namespace DevicePropertiesWebHook.Controllers
             }
             else
             {
+                if (!GetSecrets())
+                {
+                    return Ok("Request Failed");
+                }
                 // iothub-connection-device-id
                 string deviceId = data[0]["data"]["systemProperties"]["iothub-connection-device-id"].ToString();
                 Debug.WriteLine($"Device ID: {deviceId}");
@@ -128,68 +163,152 @@ namespace DevicePropertiesWebHook.Controllers
                     JProperty noUpdate = jBody.Properties().FirstOrDefault(p => p.Name.Contains("NoUpdateAvailable"));
                     JProperty appRestart = jBody.Properties().FirstOrDefault(p => p.Name.Contains("AppRestart"));
 
+                    // If the device reports no update available, this is because it has just checked with AS3 for updates, so the AS3 device data (e.g.the current OS) is up to date.
+                    // An app restart may also indicate that a software update happened, and including this facilitates testing as well as it is an easy event to trigger.
                     if (noUpdate != null || appRestart != null)
                     {
-                        if (ADToken.isAuthComplete())
+                        string token = string.Empty;
+
+                        // get the access token.
+                        bool useDeviceCodeFlow = _configuration.GetValue<bool>("MySettings:UseDeviceCodeFlow");
+                        if (useDeviceCodeFlow)
                         {
-                            Debug.WriteLine("AS3 device info...");
-                            AuthenticationResult result = await ADToken.GetAToken();
-                            if (result != null)
+                            if (ADToken.isAuthComplete())
                             {
-                                string token = result.AccessToken;
-                                string deviceData = Utils.GetAS3Data($"tenants/{AzureSphereTenantId}/devices/{deviceId}", token);
-                                DeviceInfo devInfo = JsonConvert.DeserializeObject<DeviceInfo>(deviceData);
-
-                                // https://prod.core.sphere.azure.net/v2/tenants/{tenantId}/devicegroups/{deviceGroupId}
-                                string deviceGroup = Utils.GetAS3Data($"tenants/{AzureSphereTenantId}/devicegroups/{devInfo.DeviceGroupId}", token);
-                                Debug.WriteLine(deviceGroup);
-                                DeviceGroupInfo dgInfo = JsonConvert.DeserializeObject<DeviceGroupInfo>(deviceGroup);
-
-                                // https://prod.core.sphere.azure.net/v2/tenants/{tenantId}/products/{productId}
-                                string product = Utils.GetAS3Data($"tenants/{AzureSphereTenantId}/products/{devInfo.ProductId}", token);
-                                Debug.WriteLine(product);
-                                ProductInfo prodInfo = JsonConvert.DeserializeObject<ProductInfo>(product);
-
-                                Debug.WriteLine($"Device           : {deviceId}");
-                                Debug.WriteLine($"Product          : {prodInfo.Name}");
-                                Debug.WriteLine($"Device Group     : {dgInfo.Name}");
-                                Debug.WriteLine($"Retail Eval      : {dgInfo.OsFeedType}");
-                                Debug.WriteLine($"App Update Policy: {AppUpdatePolicies[dgInfo.UpdatePolicy]}");
-
-                                string osversion = Utils.GetOSVersionForDevice(deviceId, AzureSphereTenantId, token);
-
-                                RegistryManager registryManager = RegistryManager.CreateFromConnectionString(IoTHubConnectionString);
-                                var twin = await registryManager.GetTwinAsync(deviceId);
-                                TwinState state = new TwinState
+                                Debug.WriteLine("AS3 device info...");
+                                AuthenticationResult result = await ADToken.GetAToken();
+                                if (result != null)
                                 {
-                                    Product = prodInfo.Name,
-                                    DeviceGroup = dgInfo.Name,
-                                    RetailEval = dgInfo.OsFeedType == 1 ? true : false,
-                                    AppUpdatePolicy = AppUpdatePolicies[dgInfo.UpdatePolicy],
-                                    OSVersion = string.IsNullOrEmpty(osversion) ? "Unknown" : osversion
-                                };
-
-                                var patch = $"{{ properties: {{ desired: {JsonConvert.SerializeObject(state) } }} }}";
-
-
-                                Debug.WriteLine(patch);
-
-                                await registryManager.UpdateTwinAsync(twin.DeviceId, patch, twin.ETag);
+                                    token = result.AccessToken;
+                                }
                             }
                         }
                         else
                         {
-                            Debug.WriteLine("ERROR: Auth is not complete, cannot check device id");
+                            token = await Utils.GetAS3Token();
                         }
-                    }
-                    else
-                    {
-                        Debug.WriteLine("Skip AS3 device info");
+
+                        if (string.IsNullOrEmpty(token))
+                        {
+                            return Ok("No Token");
+                        }
+
+                        TwinState state = GetDeviceTwinDataFromAS3(token, deviceId);
+
+                        // Get current twin data for 'deviceId'
+                        RegistryManager registryManager = RegistryManager.CreateFromConnectionString(IoTHubConnectionString);
+                        var twin = await registryManager.GetTwinAsync(deviceId);
+
+                        string _desiredJson = twin.Properties.Desired.ToJson();
+                        // anything changed?
+                        if (!IsDeviceTwinUpdateNeeded(_desiredJson, state))
+                        {
+                            return Ok();
+                        }
+
+                        var patch = $"{{ properties: {{ desired: {JsonConvert.SerializeObject(state) } }} }}";
+                        await registryManager.UpdateTwinAsync(twin.DeviceId, patch, twin.ETag);
                     }
                 }
+
+                return Ok();
+            }
+        }
+
+        static TwinState GetDeviceTwinDataFromAS3(string token, string deviceId)
+        {
+            string deviceData = Utils.GetAS3Data($"tenants/{AzureSphereTenantId}/devices/{deviceId}", token);
+            DeviceInfo devInfo = JsonConvert.DeserializeObject<DeviceInfo>(deviceData);
+
+            // https://prod.core.sphere.azure.net/v2/tenants/{tenantId}/devicegroups/{deviceGroupId}
+            string deviceGroup = Utils.GetAS3Data($"tenants/{AzureSphereTenantId}/devicegroups/{devInfo.DeviceGroupId}", token);
+            Debug.WriteLine(deviceGroup);
+            DeviceGroupInfo dgInfo = JsonConvert.DeserializeObject<DeviceGroupInfo>(deviceGroup);
+
+            // https://prod.core.sphere.azure.net/v2/tenants/{tenantId}/products/{productId}
+            string product = Utils.GetAS3Data($"tenants/{AzureSphereTenantId}/products/{devInfo.ProductId}", token);
+            Debug.WriteLine(product);
+            ProductInfo prodInfo = JsonConvert.DeserializeObject<ProductInfo>(product);
+
+            TwinState state = new TwinState
+            {
+                Product = prodInfo.Name,
+                DeviceGroup = dgInfo.Name,
+                RetailEval = dgInfo.OsFeedType == 1 ? true : false,
+                AppUpdatePolicy = AppUpdatePolicies[dgInfo.UpdatePolicy],
+                OSVersion = string.IsNullOrEmpty(devInfo.LastInstalledOSVersion) ? "None" : devInfo.LastInstalledOSVersion
+            };
+
+            Debug.WriteLine($"Device                    : {deviceId}");
+            Debug.WriteLine($"Product                   : {state.Product}");
+            Debug.WriteLine($"Device Group              : {state.DeviceGroup}");
+            Debug.WriteLine($"Retail Eval               : {state.RetailEval}");
+            Debug.WriteLine($"App Update Policy         : {state.AppUpdatePolicy}");
+            Debug.WriteLine($"Last Installed OS Version : {state.OSVersion}");
+
+            return state;
+        }
+
+        static bool IsDeviceTwinUpdateNeeded(string twinJson, TwinState state)
+        {
+            JObject _desired = JObject.Parse(twinJson);
+
+            string twinDesiredOS = (string)_desired["OSVersion"];
+            if (string.IsNullOrEmpty(twinDesiredOS) || twinDesiredOS != state.OSVersion)
+            {
+                Debug.WriteLine("No OS Version, or OS Version doesn't match");
+                return true;
             }
 
-            return Ok();
+            string twinDesiredProduct = (string)_desired["Product"];
+            if (string.IsNullOrEmpty(twinDesiredProduct) || twinDesiredProduct != state.Product)
+            {
+                Debug.WriteLine("No Product, or product names don't match");
+                return true;
+            }
+
+            string twinDeviceGroup = (string)_desired["DeviceGroup"];
+            if (string.IsNullOrEmpty(twinDeviceGroup) || twinDeviceGroup != state.DeviceGroup)
+            {
+                Debug.WriteLine("No Device Group, or Device Group names don't match");
+                return true;
+            }
+
+            if (!_desired.ContainsKey("RetailEval"))
+            {
+                Debug.WriteLine("RetailEval not found in JSON");
+                return true;
+            }
+
+            bool twinRetailEval = (bool)_desired["RetailEval"];
+            if (twinRetailEval != state.RetailEval)
+            {
+                Debug.WriteLine("RetailEval doesn't match");
+                return true;
+            }
+
+            string twinUpdtePolicy = (string)_desired["AppUpdatePolicy"];
+            if (string.IsNullOrEmpty(twinUpdtePolicy) || twinUpdtePolicy != state.AppUpdatePolicy)
+            {
+                Debug.WriteLine("No App Update Policy, or App Update Policy names don't match");
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool GetSecrets()
+        {
+            APIKey = Utils.GetKeyVaultString("APIKey");
+            IoTHubConnectionString = Utils.GetKeyVaultString("IoTHubConnectionString");
+            AzureSphereTenantId = Utils.GetKeyVaultString("tenantId");
+
+            if (string.IsNullOrEmpty(APIKey) || string.IsNullOrEmpty(IoTHubConnectionString) || string.IsNullOrEmpty(AzureSphereTenantId))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 
@@ -224,6 +343,10 @@ namespace DevicePropertiesWebHook.Controllers
         public string ProductId { get; set; }
         public string DeviceGroupId { get; set; }
         public int ChipSku { get; set; }
+        public string LastInstalledOSVersion { get; set; }
+        public string LastAvailableOSVersion { get; set; }
+        public object LastUpdateCheckUTC { get; set; }
+        public object LastOsUpdateUTC { get; set; }
     }
 
     public class DeviceGroupInfo
