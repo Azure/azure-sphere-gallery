83163605588305feb1abee591dbc4578464d992b
Chris Whitworth <chris.whitworth@microsoft.com>
2023-07-27 15:18:57 +0100

    Fix some logging

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -12,16 +12,38 @@ function warn()
     log "warning" "$1"
 }
 
+function debug()
+{
+    log "debug" "$1"
+}
+
+function notice()
+{
+    log "notice" "$1"
+}
+
+function group()
+{
+    echo "::group::$1"
+}
+
+function endgroup()
+{
+    echo "::endgroup::"
+}
+
 azsphere_projects=$(find . -name CMakeLists.txt -print0 | xargs -0 grep -l azsphere_configure_tools)
 
 for p in $azsphere_projects; do
     project_dir=$(dirname $p)
-    echo "::group $project_dir"
+    group project_dir
 
-    presets="$project_dir/CMakePreset.json"
+    presets="$project_dir/CMakePresets.json"
     if [ ! -e $presets ]; then
         warn "Missing $presets - skipping"
+    else
+        debug "Found $presets"
     fi
 
-    echo "::endgroup"
+    endgroup
 done
